9b4171d84d5c15b678d9e9b8810844df3ac29a46
aliddy <aidlid59@Corp.PlexusParc.com>
2023-12-03 17:47:08 -0500

    Server-side fixes

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
+import history from "connect-history-api-fallback";
 
 const app = express();
 
 app.use(cors());
+app.use(history());
 
 app.use("/", express.static("dist"));
 
